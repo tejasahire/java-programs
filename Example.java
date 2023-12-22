25087d988be9e475ff1ef18806ad3113d61c475d
tejasahire <153451424+tejasahire@users.noreply.github.com>
2023-12-22 10:42:38 +0530

    Add files via upload

---
+++ Example.java
@@ -0,0 +1,30 @@
+
+class Samsung
+{
+int ringtone,display,call;
+
+void fun1(){
+System.out.println("Fun 1 executed");	
+}
+	
+void fun2(){
+System.out.println("Fun 2 executed");	
+}
+	
+}
+
+class Example
+{
+	
+public static void main(String []args)
+{
+
+Samsung s1=new Samsung();
+ s1.fun1();
+
+}	
+	
+	
+}
+
+
